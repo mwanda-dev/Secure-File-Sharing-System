ac1e93d388af514fe9e7d89536636f799c8d19ac
Mwanda Chipongo <mwandachips00@gmail.com>
2025-07-28 17:36:18 +0200

    Implemented decrypt_file

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 use aes_gcm::{
     aead::{rand_core::RngCore, Aead, KeyInit, OsRng, Payload},
+    aes::cipher,
     Aes256Gcm, Nonce,
 };
 use sha2::{Digest, Sha256};
@@ -35,6 +36,33 @@ fn encrypt_file(path: String, password: String) -> Result<(), String> {
     out.extend(ciphertext);
 
     fs::write(format!("{}.enc", path), out).map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+#[tauri::command]
+fn decrypt_file(path: String, password: String) -> Result<(), String> {
+    let blob = fs::read(&path).map_err(|e| e.to_string())?;
+    if blob.len() < 12 {
+        return Err("Invalid File".into());
+    }
+
+    let (nonce_bytes, cipher_text) = blob.split_at(12);
+    let key = derive_key(&password);
+    let cipher = Aes256Gcm::new(&key.into());
+    let nonce = Nonce::from_slice(&nonce_bytes);
+
+    let plaintext = cipher
+        .decrypt(
+            nonce,
+            Payload {
+                msg: cipher_text,
+                aad: b"",
+            },
+        )
+        .map_err(|_| "Decryption failed")?;
+
+    let output_path = path.trim_end_matches(".enc");
+    fs::write(output_path, plaintext).map_err(|e| e.to_string())?;
     Ok(())
 }
 
